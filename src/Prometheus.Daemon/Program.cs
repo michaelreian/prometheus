bb36d8214f2eae30e903c54851dbfac6230a0f62
Michael Feliciano <michael@feliciano.com.au>
2017-05-31 23:16:50 +1000

    First

---
+++ src/Prometheus.Daemon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prometheus.Daemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
