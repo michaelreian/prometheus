b96cfce293ff84cb8975094e5ae54b08149bc065
Michael Feliciano <michael@feliciano.com.au>
2017-06-05 22:16:45 +1000

    Misc

--- scripts/build-ef.sh
+++ scripts/build-ef.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+COMMAND=${1:-default}
+
 pushd ./src/Prometheus.Core
 
-name=DatabaseMigrations$(date +%s)
+if [ "$COMMAND" = "upgrade" ]; then
+  name=DatabaseMigrations$(date +%s)
+  dotnet ef migrations add $name --startup-project ../Prometheus.Api/
+fi
 
-dotnet ef migrations add $name --startup-project ../Prometheus.Api/
 dotnet ef database update --startup-project ../Prometheus.Api/
 
 popd
