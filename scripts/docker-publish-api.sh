617da6048173a0c3d62258a8da968bd8032a14e2
Michael Feliciano <michael@feliciano.com.au>
2017-06-24 15:55:19 +1000

    home

--- scripts/docker-publish-api.sh
+++ scripts/docker-publish-api.sh
@@ -4,7 +4,7 @@ BUILD_NUMBER=$1
 
 if [ -z "$BUILD_NUMBER" ]; then usage; fi
 
-DOCKER_IMAGE_NAME=docker_registry:5000/prometheus/api
+DOCKER_IMAGE_NAME=home.mikesoft.com.au:5000/prometheus/api
 TAG=$DOCKER_IMAGE_NAME:$BUILD_NUMBER
 
 docker build -t $TAG ./artifacts/api
